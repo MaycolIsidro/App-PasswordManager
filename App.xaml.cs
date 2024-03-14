dde1829ee274cac24b50f4a9c40e81117c05326d
Maycol Isidro <maycolsebas.04@gmail.com>
2024-03-14 10:16:55 -0500

    se elimina subrayado de searchbar

--- App.xaml.cs
+++ App.xaml.cs
@@ -8,11 +8,20 @@ namespace PasswordManager
         {
             InitializeComponent();
 
-            MainPage = new NavigationPage(new LoginPage());
+            MainPage = new NavigationPage(new LoginPinPage());
             Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping(nameof(Entry), (handler, view) =>
             {
 #if __ANDROID__
                 handler.PlatformView.BackgroundTintList = Android.Content.Res.ColorStateList.ValueOf(Android.Graphics.Color.Transparent);
+#endif
+            });
+            Microsoft.Maui.Handlers.SearchBarHandler.Mapper.AppendToMapping(nameof(SearchBar), (handler, view) =>
+            {
+#if __ANDROID__          
+                Android.Widget.LinearLayout linearLayout = handler.PlatformView.GetChildAt(0) as Android.Widget.LinearLayout;
+                linearLayout = linearLayout.GetChildAt(2) as Android.Widget.LinearLayout;
+                linearLayout = linearLayout.GetChildAt(1) as Android.Widget.LinearLayout;
+                linearLayout.Background = null;
 #endif
             });
         }
