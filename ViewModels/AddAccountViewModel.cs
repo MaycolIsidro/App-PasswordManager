72cc81f6ca23810ae72c917e815b2608f487a819
Maycol Isidro <maycolsebas.04@gmail.com>
2024-03-15 16:14:14 -0500

    se agrega campo para generar contraseña

--- ViewModels/AddAccountViewModel.cs
+++ ViewModels/AddAccountViewModel.cs
@@ -9,11 +9,16 @@ namespace PasswordManager.ViewModels
     public class AddAccountViewModel : BaseViewModel
     {
         #region VARIABLES
+        private int lengthPassword;
         private int idCategoria;
         string _Password;
         public string SitioWeb { get; set; }
         public string Url { get; set; }
         public string Usuario { get; set; }
+        public bool UseNumbers { get; set; } = true;
+        public bool UseSymbols { get; set; } = true;
+        public bool UseUppers { get; set; } = true;
+        public bool UseLowers { get; set; } = true;
         private List<string> Errors { get; set; }
         readonly TextEncript encript = new();
         readonly SQLiteHelper db = new();
@@ -35,11 +40,16 @@ namespace PasswordManager.ViewModels
             get { return idCategoria; }
             set { SetValue(ref idCategoria, value); }
         }
+        public int LengthPassword
+        {
+            get { return lengthPassword; }
+            set { SetValue(ref lengthPassword, value); }
+        }
         #endregion
         #region PROCESOS
         private void GeneratePassword()
         {
-            Password = encript.GeneratePassword();
+            Password = TextEncript.GeneratePassword(LengthPassword,UseNumbers,UseSymbols,UseUppers,UseLowers);
         }
         private void SeleccionarCategoria(string idCategoria)
         {
