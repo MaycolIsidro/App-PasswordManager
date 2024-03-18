c0748e27cba48afdfa2f38159003b4584f8bcfc6
Maycol Isidro <maycolsebas.04@gmail.com>
2024-03-18 09:33:04 -0500

    se agrega metodo para ocultar y mostrar generador de contraseña

--- Views/AddAccountPage.xaml.cs
+++ Views/AddAccountPage.xaml.cs
@@ -3,9 +3,17 @@ using PasswordManager.ViewModels;
 namespace PasswordManager.Views;
 public partial class AddAccountPage
 {
+	bool usePasswordGenerator;
 	public AddAccountPage(int idCategoria)
 	{
 		InitializeComponent();
 		BindingContext = new AddAccountViewModel(idCategoria);
 	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+		usePasswordGenerator = !usePasswordGenerator;
+		BtnUsePasswordGenerator.IsVisible = !usePasswordGenerator;
+		StckPasswordGenerator.IsVisible = usePasswordGenerator;
+    }
 }
