0c9a16ec9ba281420a1dcd321382a35646792d14
Maycol Isidro <maycolsebas.04@gmail.com>
2024-03-22 10:38:10 -0500

    se establece pagina de inicio

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,29 +1,25 @@
 ﻿using PasswordManager.Views;
 
-namespace PasswordManager
+namespace PasswordManager;
+public partial class App : Application
 {
-    public partial class App : Application
+    public App()
     {
-        public App()
-        {
-            InitializeComponent();
+        InitializeComponent();
 
-            MainPage = new NavigationPage(new LoginPinPage());
-            Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping(nameof(Entry), (handler, view) =>
-            {
+        MainPage = new NavigationPage(new LoginPage());
 #if __ANDROID__
-                handler.PlatformView.BackgroundTintList = Android.Content.Res.ColorStateList.ValueOf(Android.Graphics.Color.Transparent);
+        Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping(nameof(Entry), (handler, view) =>
+        {
+            handler.PlatformView.BackgroundTintList = Android.Content.Res.ColorStateList.ValueOf(Android.Graphics.Color.Transparent);
+        });
+        Microsoft.Maui.Handlers.SearchBarHandler.Mapper.AppendToMapping(nameof(SearchBar), (handler, view) =>
+        {      
+            Android.Widget.LinearLayout linearLayout = handler.PlatformView.GetChildAt(0) as Android.Widget.LinearLayout;
+            linearLayout = linearLayout.GetChildAt(2) as Android.Widget.LinearLayout;
+            linearLayout = linearLayout.GetChildAt(1) as Android.Widget.LinearLayout;
+            linearLayout.Background = null;
+        });
 #endif
-            });
-            Microsoft.Maui.Handlers.SearchBarHandler.Mapper.AppendToMapping(nameof(SearchBar), (handler, view) =>
-            {
-#if __ANDROID__          
-                Android.Widget.LinearLayout linearLayout = handler.PlatformView.GetChildAt(0) as Android.Widget.LinearLayout;
-                linearLayout = linearLayout.GetChildAt(2) as Android.Widget.LinearLayout;
-                linearLayout = linearLayout.GetChildAt(1) as Android.Widget.LinearLayout;
-                linearLayout.Background = null;
-#endif
-            });
-        }
     }
 }
